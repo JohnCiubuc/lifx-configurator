ca8c62976940ed3d46dff4c3074d08db61c6658b
John Ciubuc <john.ciubuc@ttuhsc.ed>
2020-07-23 15:55:44 -0500

    Added power control option

--- main.cpp
+++ main.cpp
@@ -44,14 +44,33 @@ std::string hexstr2byte(std::string hex)
 
 }
 
+enum messages
+{
+    NullType = -1,
+    SetColor,
+    SetPower
+};
+
+struct payload_s{
+
+  std::string brightnessLevel;
+  std::string powerLevel;
+};
 // Builds frame header
 // But also builds command
-std::string buildHeader()
+std::string buildHeader(messages PacketType)
 {
     std::string frameHeader="003400000000000000000000000000000000000000000000000000000000";
     // Adds function and other stuff. Change if interested
-    // Currently only does SetColor function (https://lan.developer.lifx.com/v2.0/docs/light-messages#section-setcolor-102)
-    frameHeader += "66000000";
+    switch(PacketType)
+    {
+    case SetColor:
+        frameHeader += "66000000";
+        break;
+    case SetPower:
+        frameHeader += "75000000";
+        break;
+    }
     std::string byteStr;
     for(int i = 0; i < frameHeader.length(); i+=2)
         byteStr += bytePrepend(std::string(std::string(1,frameHeader[i]) + std::string(1,frameHeader[i+1])));
@@ -59,11 +78,58 @@ std::string buildHeader()
     return byteStr;
 }
 
+std::string buildPayload(messages PacketType, payload_s Payload)
+{
+    std::string payload;
+
+    switch(PacketType)
+    {
+    case SetColor:
+        payload ="005555FFFF";
+
+        payload += Payload.brightnessLevel;
+
+        // Rest of settings for SetColor function
+        payload += "AC0D00040000";
+
+        break;
+    case SetPower:
+        payload += Payload.powerLevel;
+        payload += "00040000";
+        break;
+    }
+
+    std::string packet;
+  // Builds packet
+  for(int i = 0; i < payload.length(); i+=2)
+      packet += bytePrepend(std::string(std::string(1,payload[i]) + std::string(1,payload[i+1])));
+    return packet;
+}
+
+
+void lifxHelpMenu()
+{
+    std::cout <<"Usage: lifx-packet-builder [OPTION] ... [VALUE]\n";
+    std::cout << "\tReturns packet required to set desired BRIGHTNESS to a LifX™ bulb\n\n";
+    std::cout << "Options:\n\n";
+    std::cout << "\t-b, --brightness\n";
+    std::cout << "\t\t\t Accepts a BRIGHTNESS value from 0 to 100.";
+    std::cout << "\t-p, --power\n";
+    std::cout << "\t\t\t Accepts a POWER value of 0 (off) or 1 (on).";
+
+    std::cout << std::endl;
+}
+
+void packetType_Error()
+{
+    std::cout << "ERROR: More than 1 Packet Type requested. Please choose only one Packet Type to generate\n\n";
+    lifxHelpMenu();
+}
+
 int main(int argc, char ** argv)
 {
-
-    // Default, 100%
-    std::string brightnessLevel = flipHex(n2hexstr(65535,4));
+    messages PacketType = NullType;
+    payload_s Payload;
 
     // Parser:
     if(argc > 1 && argc % 2 == 1)
@@ -74,29 +140,52 @@ int main(int argc, char ** argv)
             std::string args = std::string(argv[argi]);
             if(args.compare("-b") == 0 || args.compare("--brightness") == 0)
             {
+                if (PacketType != NullType)
+                {
+                    packetType_Error();
+                    return 0;
+                }
+                PacketType = SetColor;
+
                 int bLevel;
                 sscanf(argv[argi+1], "%d", &bLevel);
 
                 bLevel = static_cast<int>(65535*(bLevel / 100.));
 
-                brightnessLevel = flipHex(n2hexstr(bLevel,4));
+                Payload.brightnessLevel = flipHex(n2hexstr(bLevel,4));
+            }
+            if(args.compare("-p") == 0 || args.compare("--power") == 0)
+            {
+                if (PacketType != NullType)
+                {
+                    packetType_Error();
+                    return 0;
+                }
+                PacketType = SetPower;
+                int bLevel;
+                sscanf(argv[argi+1], "%d", &bLevel);
+
+                if(bLevel == 1){
+                    Payload.powerLevel = flipHex(n2hexstr(65535,4));
+                }
+                else if(bLevel ==0){
+                    Payload.powerLevel = flipHex(n2hexstr(0,4));
+                }
+                else{
+                    packetType_Error();
+                    return 0;
+                }
             }
         }
-        
-        std::string packet = buildHeader();
 
-        // Other settings for SetColor function
-        std::string payload ="005555FFFF";
+        if (PacketType == NullType)
+        {
+            packetType_Error();
+            return 0;
+        }
+        std::string packet = buildHeader(PacketType);
 
-        payload += brightnessLevel;
-
-        // Rest of settings for SetColor function
-        payload += "AC0D00040000";
-
-
-        // Builds packet
-        for(int i = 0; i < payload.length(); i+=2)
-            packet += bytePrepend(std::string(std::string(1,payload[i]) + std::string(1,payload[i+1])));
+        packet += buildPayload(PacketType, Payload);
 
         // Gets packet size for packet size header
         int packetSize = 0;
@@ -129,11 +218,13 @@ int main(int argc, char ** argv)
     }
     else
     {
-        std::cout <<"Usage: lifx-packet-builder [OPTION] ... [VALUE]\n";
+        std::cout <<"Usage: lifx-configurator [OPTION] ... [VALUE]\n";
         std::cout << "\tReturns packet required to set desired BRIGHTNESS to a LifX™ bulb\n\n";
         std::cout << "Options:\n\n";
         std::cout << "\t-b, --brightness\n";
-        std::cout << "\t\t\t Accepts a BRIGHTNESS value from 0 to 100.";
+        std::cout << "\t\t\t Accepts a BRIGHTNESS value from 0 to 100.\n";
+        std::cout << "\t-p, --power\n";
+        std::cout << "\t\t\t Accepts a POWER value of 0 (off) or 1 (on).\n";
 
         std::cout << std::endl;
         return 0;
